8d0fd8d5e7c89f4f4aae401ffed9442dd7dca9fd
Zelda Hessler <zelda@Zeldas-MacBook.local>
2015-12-16 11:53:44 -0600

    Created a meteor project and added some packages

--- adventure.js
+++ adventure.js
@@ -1,31 +1,56 @@
 Scenes = new Mongo.Collection("scenes");
 Commands = new Mongo.Collection("commands");
 
-Scenes.insert ({
-  scene : {
-    title : "home",
-    setting: "the internet",
-  }
+Router.configure({
+  layoutTemplate: 'layout'
+});
+Router.route('/', function() {
+  this.render('main');
+  console.log(this);
 });
 
-Commands.insert ({
-    look : {
-      command : "Look",
-      example : "LOOK AT object",
-      helpText : "Use look to look",
-   }
-    use : {
-      command : "Use",
-      example : "USE object",
-      helpText : "Use use to use",
-   }
-    take : {
-      command : "Take",
-      example : "TAKE object",
-      helpText : "Use take to take",
-   }
-    talk : {
-      command : "Talk",
-      example : "TALK TO person",
-      helpText : "Use talk to talk",}
-});
+function reuseableTyped(elementClass, whatToType) {
+  (elementClass).typed({
+    strings: [whatToType],
+    typeSpeed: 0
+  });
+}
+
+if (Meteor.isClient) {
+  // Template.main.helpers({
+  //   introText : reuseableTyped(".intro", "This is test text for the intro page")
+  // });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function() {
+    if (!Scenes.findOne()) {
+      Scenes.insert({
+        title: "home",
+        setting: "the internet",
+      });
+    }
+    if (!Commands.findOne()) {
+      Commands.insert({
+        command: "look",
+        example: "LOOK AT object",
+        helpText: "Use look to look",
+      });
+      Commands.insert({
+        command: "use",
+        example: "USE object",
+        helpText: "Use use to use",
+      });
+      Commands.insert({
+        command: "take",
+        example: "TAKE object",
+        helpText: "Use take to take",
+      });
+      Commands.insert({
+        command: "talk",
+        example: "TALK TO person",
+        helpText: "Use talk to talk",
+      });
+    }
+  });
+}
