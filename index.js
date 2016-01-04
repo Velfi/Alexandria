5462da4520f877ca5f044058074131d72b236ec2
velfi <zelda.hessler@gmail.com>
2016-01-04 14:12:34 -0600

    More things work even better now
    Add user input sanitizer
    Modify styling to look a bit nicer
    Fix parser to actually work.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+// # Startup stuff # //
+$('.display-container').replaceWith("<h1>You look tired.</h1><h2>Why don't you come in and have a seat.</h2><p>I wasn't sure I'd have any visitors today.</p><p>How's about I tell you a story?</p><p>I have one in mind, but if you can think of one you'd like to here instead, just say so.</p><br/><p>(To load a story, just type 'load (story)' and press enter or, if you like to hear the story they had in mind, just type 'say ok' and press enter.)</p>")
+
+// # Global vars # //
+var lastCommand = $(".submit-on-enter").val();
+
+// # Functions # //
+$(".submit-on-enter").keydown(function(event) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    lastCommand = $(".submit-on-enter").val();
+    $(".submit-on-enter").val("");
+    parser(lastCommand);
+  }
+});
+
+function inputSanitizer(commandInput) {
+  return commandInput.toLowerCase().split(" ");
+}
+
+function parser(stringsToParse) {
+  switch (inputSanitizer(stringsToParse)[0]) {
+    case "help":
+      help();
+      break;
+    default:
+      console.log("That input is invalid.");
+  }
+}
+
+function help(){console.log("you have successfully entered the help command.")}
+
+
+// function load(adventure_game){
+//
+// }
+// function help(){
+//
+// }
+
 //     if (!Scenes.findOne()) {
 //       Scenes.insert({
 //         title: "home",
@@ -27,23 +67,4 @@
 //       });
 //     }
 //   });
-// }
-
-$(document).ready(function() {
-  $('.display-container').replaceWith("<h2>You look tired. Why don't you come in and have a seat.</h2><p>I wasn't sure I'd have any visitors today.</p>")
-  $('.submit-on-enter').keydown(function(event) {
-    if (event.keyCode == 13) {
-      var lastCommand = $(".submit-on-enter").val();
-      alert(lastCommand);
-      $(".input").reset();
-      return false;
-    }
-  });
-});
-
-// function load(adventure_game){
-//
-// }
-// function help(){
-//   
 // }
