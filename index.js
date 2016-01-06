112b99afe94e051e6199b02c913c4a42d6f3c42a
velfi <zelda.hessler@gmail.com>
2016-01-05 19:09:27 -0600

    So much progress, and a bit of confusion.
    So, I've got the parser working (more or less) and I've started adding
    the different commands. I'm currently working on the move / scene loading
    command.

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
-// # Startup stuff # //
-$('.display-container').replaceWith("<h1>You look tired.</h1><h2>Why don't you come in and have a seat.</h2><p>I wasn't sure I'd have any visitors today.</p><p>How's about I tell you a story?</p><p>I have one in mind, but if you can think of one you'd like to here instead, just say so.</p><br/><p>(To load a story, just type 'load (story)' and press enter or, if you like to hear the story they had in mind, just type 'say ok' and press enter.)</p>")
-
-// # Global vars # //
-var lastCommand = $(".submit-on-enter").val();
-
+"use strict";
+localStorage.clear();
+// imports
+var scenes = $.ajax({
+  type: "GET",
+  url: "scenes.json",
+  async: false,
+  beforeSend: function(x) {
+    if (x && x.overrideMimeType) {
+      x.overrideMimeType("application/j-son;charset=UTF-8");
+    }
+  },
+  dataType: "json",
+  success: function(data) {
+    return data;
+  }
+});
+var help_files = $.ajax({
+  type: "GET",
+  url: "help.json",
+  async: false,
+  beforeSend: function(x) {
+    if (x && x.overrideMimeType) {
+      x.overrideMimeType("application/j-son;charset=UTF-8");
+    }
+  },
+  dataType: "json",
+  success: function(data) {
+    return data;
+  }
+});
+// displays initial scene.
+$("div.display-container").html(scenes.responseJSON.scene0.scene_text);
+var currentScene = "scene0";
 // # Functions # //
+// submit the users input on enter keypress
 $(".submit-on-enter").keydown(function(event) {
   if (event.keyCode == 13) {
     event.preventDefault();
-    lastCommand = $(".submit-on-enter").val();
+    var lastCommand = $(".submit-on-enter").val();
     $(".submit-on-enter").val("");
+    console.log(inputSanitizer(lastCommand));
     parser(lastCommand);
   }
 });
@@ -19,52 +49,79 @@ function inputSanitizer(commandInput) {
 }
 
 function parser(stringsToParse) {
+  $(".submit-on-enter").attr("placeholder", "Last Command: " + stringsToParse);
   switch (inputSanitizer(stringsToParse)[0]) {
     case "help":
       help();
       break;
+    case "load":
+      loadStory();
+      break;
+    case "go":
+      moveTo(inputSanitizer(stringsToParse)[1]);
+      break;
+    case "look":
+      look();
+      break;
+    case "use":
+      use();
+      break;
+    case "take":
+      take();
+      break;
+    case "talk":
+      talk();
+      break;
+      // case "theme":
+      //   console.log(inputSanitizer(stringsToParse)[1]);
+      //   themeSwitcher(inputSanitizer(stringsToParse)[1]);
+      //   break;
     default:
       console.log("That input is invalid.");
   }
 }
 
-function help(){console.log("you have successfully entered the help command.")}
+// * In-game commands * //
+function help() {
+  // display the help screen
+  $(".display-container").html(help_files.responseJSON.help.topic_content);
+}
 
+function loadStory(story) {
+  if (currentScene === scene0) {
+    // put in code for loading different story files.
+    return true;
+    // scenes.responseJSON.scene0.scene_text
+  } else {
+    console.log("You can only load levels from the main screen.");
+  }
+}
 
-// function load(adventure_game){
-//
-// }
-// function help(){
-//
-// }
+function moveTo(scene) {
+  if (scenes.responseJSON.hasOwnProperty(scene)) {
+    console.log(scenes.responseJSON[scene].scene_text);
+  }
+}
 
-//     if (!Scenes.findOne()) {
-//       Scenes.insert({
-//         title: "home",
-//         setting: "the internet",
-//       });
-//     }
-//     if (!Commands.findOne()) {
-//       Commands.insert({
-//         command: "look",
-//         example: "LOOK AT object",
-//         helpText: "Use look to look",
-//       });
-//       Commands.insert({
-//         command: "use",
-//         example: "USE object",
-//         helpText: "Use use to use",
-//       });
-//       Commands.insert({
-//         command: "take",
-//         example: "TAKE object",
-//         helpText: "Use take to take",
-//       });
-//       Commands.insert({
-//         command: "talk",
-//         example: "TALK TO person",
-//         helpText: "Use talk to talk",
-//       });
-//     }
-//   });
+function look() {
+  console.log("you have successfully entered the look command.");
+  currentScene
+}
+
+function use() {
+  console.log("you have successfully entered the use command.");
+}
+
+function take() {
+  console.log("you have successfully entered the take command.");
+}
+
+function talk() {
+  console.log("you have successfully entered the talk command.");
+}
+
+// couldn"t get it to work, I"ll come back later.
+// function themeSwitcher() {
+//   $("body").css("background-color", "$base2");
+//   $("input").css("background-color", "$base2");
 // }
